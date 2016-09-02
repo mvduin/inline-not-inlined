f2eeacfe15c8a9c371fcec349c979b8cd53c83f5
Matthijs van Duin <matthijsvanduin@gmail.com>
2016-09-02 03:01:06 +0200

    use extern inline in foo99.c
    See its comment for explanation.

--- foo99.c
+++ foo99.c
@@ -1,4 +1,7 @@
 #define UNIT "foo99.c"
 #include "foo.h"
 
-void foo();
+// The "extern inline" is optional but ensures you'll get a compiler warning
+// if no definition is provided (typically as inline function in a header).
+// This is more useful then the linker errors further down the road.
+extern inline void foo();
